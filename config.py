3756d89b013b0110925722c7eda0b1ec032fa5cc
Kacper Paczos <kacper-paczos@linux.pl>
2023-12-23 21:21:26 +0100

    Switched to pydantic settings

--- config.py
+++ config.py
@@ -1,2 +1,12 @@
-version = "0.0.3"
-debug = True
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    version: str = "0.0.3"
+    debug: bool = True
+
+# load and parse settings
+settings = Settings()
+
+# access variables
+version = settings.version
+debug = settings.debug
