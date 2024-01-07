f9b699f9fa66511bf719d169121aa1c1e037209b
Kacper Paczos <kacper-paczos@linux.pl>
2024-01-07 17:08:31 +0100

    Switched to argparse

--- __main__.py
+++ __main__.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
 import sys
+import argparse
 from bennet import Bennet
 from rich import print
 from common.config import debug
 from common.common import ic
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Bennet is a tool inspired by Yarn, designed specifically for building Linux projects in C/C++, Python, JavaScript, and GTK environments. It seamlessly manages workspaces, packages, and builds for diverse language and build system combinations.")
+    parser.add_argument("operation", choices=['build', 'update', 'install', 'init', 'help'], nargs='?', default='init', help="Operation to perform")
+    args = parser.parse_args()
+    
     bennet = Bennet()
-    if len(sys.argv) > 1:
-        function_name = sys.argv[1]
-        if function_name in ['-h', 'help']:
-            bennet.help()
-        else:
-            function = getattr(bennet, function_name, None)
-            if function:
-                function()
-            else:
-                if not debug:
-                    ic(f"No such function: {function_name}")
-            
+    operation = args.operation
+    
+    if operation == 'build':
+        bennet.build()
+    elif operation == 'update':
+        bennet.updated()
+    elif operation == 'install':
+        bennet.install()
+    elif operation == 'init':
+        bennet.init()
+    elif operation == 'help':
+        bennet.help()
     else:
         if not debug: 
-            ic("No function name provided in command line arguments.")
+            ic("No valid operation provided in command line arguments.")
+
 
