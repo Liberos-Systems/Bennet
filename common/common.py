f9e0130d0df4ba74951217fc4081fb590ffe3ce7
Kacper Paczos <kacper-paczos@linux.pl>
2024-01-08 23:33:52 +0100

    More info when debug is true

--- common/common.py
+++ common/common.py
@@ -1,9 +1,13 @@
 from icecream import ic as original_ic
+from common.config import debug
 import datetime
 
 def ic(*args, **kwargs):
-    current_time = datetime.datetime.now().strftime("%H:%M:%S")
-    log_message = f"<<{current_time}>> " + " ".join(map(str, args))
+    if debug:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        log_message = f"<<{current_time}>> " + " ".join(map(str, args))
+    else:
+        log_message = " ".join(map(str, args))
     try:
         original_ic(log_message, **kwargs)
     except Exception as e:
