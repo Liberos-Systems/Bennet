c3189744e26b62bc0e9ee755e582b79235f7324d
Kacper Paczos <kacper-paczos@linux.pl>
2024-01-17 23:56:40 +0100

    Added extra arguments

--- __main__.py
+++ __main__.py
@@ -8,24 +8,24 @@ from common.common import ic
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Bennet is a tool inspired by Yarn, designed specifically for building Linux projects in C/C++, Python, JavaScript, and GTK environments. It seamlessly manages workspaces, packages, and builds for diverse language and build system combinations.")
-    parser.add_argument("operation", nargs='?', default='init', help="Operation to perform")
+    parser.add_argument("operation", nargs='?', help="Operation to perform")
+    parser.add_argument("extra_args", nargs=argparse.REMAINDER, help="Extra arguments for the operation")
     args = parser.parse_args()
     
     bennet = Bennet()
-    operation = args.operation
+    operation = args.operation if args.operation else 'init'
     
     if operation == 'build':
-        bennet.build()
+        bennet.build(*args.extra_args)
     elif operation == 'update':
-        bennet.update()
+        bennet.update(*args.extra_args)
     elif operation == 'install':
-        bennet.install()
+        bennet.install(*args.extra_args)
     elif operation == 'init':
-        bennet.init()
+        bennet.init(*args.extra_args)
     elif operation == 'help':
-        bennet.help()
+        bennet.help(*args.extra_args)
     else:
-        bennet.script(operation)
-            
+        bennet.script(operation, *args.extra_args)
 
 
